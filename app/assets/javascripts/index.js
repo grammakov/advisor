f82b29867e52ba5c86d8c354d6ad1a1da802323e
Alexander Kravchenko <snowacat@gmail.com>
2016-08-18 19:31:18 +0300

    initial commit

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -14,7 +14,30 @@ $(function(){
             $(this).prev().removeClass('minus')
         }
 
-        // $a.not(this).next().slideUp(500); // Hide next slide
+        // $a.not(this).next().slideUp(500); // Hide next slide. Cool option but...
         $(this).next().slideToggle(500);
+    });
+});
+
+
+$(document).ready(function() {
+    // $('.second-box').hide();
+    $('.second-box, .third-box, .last-box').hide();
+
+    $('.icon-box').click(function() {
+        // Hide previous box
+        var activeBox = $('.active-box');
+        var boxForHideClass = activeBox.attr('data-type');
+        $('.' + boxForHideClass).hide();
+
+        // Set new active icon
+        activeBox.removeClass('active-box');
+        $(this).addClass('active-box');
+
+        // Show text box
+        var className = $(this).attr('data-type');
+        $('.' + className).fadeIn(1000);
+
+
     });
 });
